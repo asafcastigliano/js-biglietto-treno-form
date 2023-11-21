2d1f9ef8b674d7ddeb2b5ff26cea560fe9bdbfd1
asafcastigliano <asafcastigliano@gmail.com>
2023-11-21 18:36:50 +0100

    finito javascript

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
+const button = document.querySelector(".genera");
+button.addEventListener("click", function(){
+    let nome = document.getElementById("nome").value;
+    let km = document.getElementById("km").value;
+    let age = document.getElementById("age").value;
 
+    document.getElementById("passeggero").innerHTML = "<b>NOME PASSEGGERO<br>" + nome + "<b>";
 
-const PREZZO = 0.21;
+    document.getElementById("carrozza-titolo").innerHTML = "<b>Carrozza<b>";
+    document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10) + 1;
 
-let totale = km * PREZZO;
+    document.getElementById("cp-titolo").innerHTML = "<b>Codice CP  <b>";
+    document.getElementById("cp").innerHTML = Math.floor(Math.random() * 90000) + 10000;
 
-let sconto = 0;
+    const PREZZO = 0.21;
+    let totale = km * PREZZO;
+    let sconto = 0;
 
-if(age < 18){
-    sconto = totale * 0.2;
-} else if (age >= 65){
-    sconto = totale * 0.4;
-}
+    document.getElementById("offerta-titolo").innerHTML = "<b>Offerta<b>";
 
-totale = (totale - sconto).toFixed(2);
+    if(age == "minorenne"){
+        document.getElementById("offerta").innerHTML = "Biglietto Minorenni";
+        sconto = totale * 0.2;
+    } else if(age == "anziano"){
+        document.getElementById("offerta").innerHTML = "Biglietto Over 65";
+        sconto = totale * 0.4;
+    } else {
+        document.getElementById("offerta").innerHTML = "Biglietto Standard";
+    }
 
-console.log(totale + "€");
+    document.getElementById("costo-titolo").innerHTML = "<b>Costo biglitetto<b>";
+
+    document.getElementById("costo").innerHTML = totale = (totale - sconto).toFixed(2) + "€";
+})
